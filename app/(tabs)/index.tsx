ce70739aa127cc7f1000bc7e3a7a762ddf55aaf9
Diego <diegofreeman78@gmail.com>
2024-01-10 12:02:57 +0100

    Added native notify

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,6 +16,8 @@ import { ErrorComponent } from '../../components/ErrorComponent';
 import { Button } from 'react-native';
 import axios from 'axios';
 import { useStoreId } from '../../hooks/useAuth';
+import { useToast } from 'react-native-toast-notifications';
+import registerNNPushToken from 'native-notify';
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
@@ -37,39 +39,40 @@ export default function TabOneScreen() {
 
   const { id } = useStoreId();
 
+  registerNNPushToken(18094, 'XrpSQHg242Xgsh6GkilQD8');
   const [products, setProducts] = useState<PickUp[] | undefined>(data);
-
+  const toast = useToast();
   const [expoPushToken, setExpoPushToken] = useState<string | undefined>('');
   const [notification, setNotification] =
     useState<Notifications.Notification>();
   const notificationListener = useRef<Notifications.Subscription>();
   const responseListener = useRef<Notifications.Subscription>();
 
-  useEffect(() => {
-    registerForPushNotificationsAsync(id).then((token) =>
-      setExpoPushToken(token)
-    );
-    if (notificationListener?.current) {
-      notificationListener.current =
-        Notifications?.addNotificationReceivedListener((notification) => {
-          setNotification(notification);
-        });
-    }
+  // useEffect(() => {
+  //   registerForPushNotificationsAsync(id).then((token) =>
+  //     setExpoPushToken(token)
+  //   );
+  //   if (notificationListener?.current) {
+  //     notificationListener.current =
+  //       Notifications?.addNotificationReceivedListener((notification) => {
+  //         setNotification(notification);
+  //       });
+  //   }
 
-    responseListener.current =
-      Notifications?.addNotificationResponseReceivedListener((response) => {
-        return response;
-      });
+  //   responseListener.current =
+  //     Notifications?.addNotificationResponseReceivedListener((response) => {
+  //       return response;
+  //     });
 
-    return () => {
-      Notifications?.removeNotificationSubscription(
-        notificationListener?.current as any
-      );
-      Notifications?.removeNotificationSubscription(
-        responseListener?.current as any
-      );
-    };
-  }, []);
+  //   return () => {
+  //     Notifications?.removeNotificationSubscription(
+  //       notificationListener?.current as any
+  //     );
+  //     Notifications?.removeNotificationSubscription(
+  //       responseListener?.current as any
+  //     );
+  //   };
+  // }, []);
 
   if (isError || isPaused) {
     return <ErrorComponent refetch={refetch} />;
@@ -91,20 +94,26 @@ export default function TabOneScreen() {
   }
 
   const sendPushNotification = async () => {
+    if (!expoPushToken) {
+      return toast.show('Something went wrong, please try again later', {
+        type: 'danger',
+        placement: 'bottom',
+        duration: 4000,
+        animationType: 'slide-in',
+      });
+    }
     await fetch('https://exp.host/--/api/v2/push/send', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        to: [expoPushToken],
+        to: expoPushToken,
         title: 'Your title',
         body: 'Body of your push notification',
       }),
     });
   };
-  console.log(data);
-  console.log(products);
 
   return (
     <View style={[{ flex: 1, paddingTop: 20, backgroundColor: 'white' }]}>
@@ -135,51 +144,51 @@ export default function TabOneScreen() {
   );
 }
 
-async function registerForPushNotificationsAsync(id: string) {
-  let token;
+// async function registerForPushNotificationsAsync(id: string) {
+//   let token;
 
-  if (Platform?.OS === 'android') {
-    await Notifications?.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
-  }
+//   if (Platform?.OS === 'android') {
+//     await Notifications?.setNotificationChannelAsync('default', {
+//       name: 'default',
+//       importance: Notifications.AndroidImportance.MAX,
+//       vibrationPattern: [0, 250, 250, 250],
+//       lightColor: '#FF231F7C',
+//     });
+//   }
 
-  if (Device.isDevice) {
-    const { status: existingStatus } =
-      await Notifications?.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications?.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== 'granted') {
-      Alert.alert(
-        "Permissions weren't granted!",
-        'Notifications need permission'
-      );
-      return;
-    }
+//   if (Device.isDevice) {
+//     const { status: existingStatus } =
+//       await Notifications?.getPermissionsAsync();
+//     let finalStatus = existingStatus;
+//     if (existingStatus !== 'granted') {
+//       const { status } = await Notifications?.requestPermissionsAsync();
+//       finalStatus = status;
+//     }
+//     if (finalStatus !== 'granted') {
+//       Alert.alert(
+//         "Permissions weren't granted!",
+//         'Notifications need permission'
+//       );
+//       return;
+//     }
 
-    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
-    token = (
-      await Notifications.getExpoPushTokenAsync({
-        projectId: Constants?.expoConfig?.extra?.eas.projectId,
-      })
-    ).data;
-    const res = await axios.post(
-      `https://247api.netpro.software/api.aspx?api=deliveryupdateagentref&agentid=${id}&agentRef=${token} `
-    );
+//     // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+//     token = (
+//       await Notifications.getExpoPushTokenAsync({
+//         projectId: Constants?.expoConfig?.extra?.eas.projectId,
+//       })
+//     ).data;
+//     const res = await axios.post(
+//       `https://247api.netpro.software/api.aspx?api=deliveryupdateagentref&agentid=${id}&agentRef=${token} `
+//     );
 
-    console.log(res?.data, 'token');
-  } else {
-    alert('Must use physical device for Push Notifications');
-  }
+//     console.log(res?.data, 'token');
+//   } else {
+//     alert('Must use physical device for Push Notifications');
+//   }
 
-  return token;
-}
+//   return token;
+// }
 
 const styles = StyleSheet.create({
   title: {
